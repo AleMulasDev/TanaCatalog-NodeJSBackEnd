7181278504fa4e0d61677396b891702e0c6cec2d
BattleIsOver <34491874+BattleIsOver@users.noreply.github.com>
2020-04-05 23:31:19 +0200

    Standardized some variable names

--- models/SectionGame.js
+++ models/SectionGame.js
@@ -1,28 +1,28 @@
 class SectionGames{
-  constructor({id, game_id, section_id, is_new, acquisition_date, origin, propriety, holder_id}){
+  constructor({id, gameID, sectionID, isNew, acquisitionDate, origin, propriety, holderID}){
     this.id = id;
-    this.game_id = game_id;
-    this.section_id = section_id;
-    this.is_new = is_new;
-    this.acquisition_date = acquisition_date;
+    this.gameID = gameID;
+    this.sectionID = sectionID;
+    this.isNew = isNew;
+    this.acquisitionDate = acquisitionDate;
     this.origin = origin;
     this.propriety = propriety;
-    this.holder_id = holder_id;
+    this.holderID = holderID;
   }
 }
 
 class SectionGamesQuery{
   constructor({id, game_id, section_id, is_new, acquisition_date, origin, propriety, holder_id, holder_title, game_title}){
     this.id = id;
-    this.game_id = game_id;
-    this.section_id = section_id;
-    this.is_new = is_new;
-    this.acquisition_date = acquisition_date;
+    this.gameID = game_id;
+    this.sectionID = section_id;
+    this.isNew = is_new;
+    this.acquisitionDate = acquisition_date;
     this.origin = origin;
     this.propriety = propriety;
-    this.holder_id = holder_id;
-    this.holder_title = holder_title;
-    this.game_title = game_title;
+    this.holderID = holder_id;
+    this.holderTitle = holder_title;
+    this.gameTitle = game_title;
   }
 }
 module.exports = {SectionGames, SectionGamesQuery};
